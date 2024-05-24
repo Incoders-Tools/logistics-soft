99a6a76da7715d4ab1dd1ee08dd38077957960be
Patricio-Montes <montes_patricio@yahoo.com.ar>
2024-05-24 17:52:09 -0300

    Added: Delivery note changes.

--- src/app/domain/artifacts/deliverynotes/component/delivery-note.component.ts
+++ src/app/domain/artifacts/deliverynotes/component/delivery-note.component.ts
@@ -3,9 +3,15 @@ import { Table } from 'primeng/table';
 import { ConfirmationService, MenuItem, MessageService } from 'primeng/api';
 import { Product } from 'src/app/demo/api/product';
 import { ProductService } from 'src/app/demo/service/product.service';
+import { CountryService } from 'src/app/demo/service/country.service';
 
 interface expandedRows {
   [key: string]: boolean;
+}
+
+interface SectionVisibility {
+  showMasiva: boolean;
+  showManual: boolean;
 }
 
 @Component({
@@ -26,17 +32,74 @@ export class DeliveryNoteComponent implements OnInit {
 
   expandedRows: expandedRows = {};
 
+  sectionVisibility: SectionVisibility = {
+    showMasiva: false,
+    showManual: false
+  };
+
+  // Arts Subtable
+
+  productDialog: boolean = false;
+
+  deleteProductDialog: boolean = false;
+
+  deleteProductsDialog: boolean = false;
+
+  product: Product = {};
+
+  selectedProducts: Product[] = [];
+
+  submitted: boolean = false;
+
+  cols: any[] = [];
+
+  statuses: any[] = [];
+
+  rowsPerPageOptions = [5, 10, 20];
+
+  selectedCountryAdvanced: any[] = [];
+
+  countries: any[] = [];
+
+  filteredCountries: any[] = [];
+
   @ViewChild('filter') filter!: ElementRef;
 
-  constructor(private messageService: MessageService, private productService: ProductService) { }
+  constructor(private messageService: MessageService, private productService: ProductService, private countryService: CountryService) { }
 
   ngOnInit(): void {
     this.items = [
-      { label: 'Confeccion Manual', icon: 'pi pi-external-link' },
-      { label: 'Confeccion Masiva', icon: 'pi pi-bookmark' }
+      { 
+        label: 'Confeccion Manual', 
+        icon: 'pi pi-book', 
+        command: () => this.save('Confeccion Manual') 
+      },
+      { 
+        label: 'Confeccion Masiva', 
+        icon: 'pi pi-sync', 
+        command: () => this.save('Confeccion Masiva') 
+      }
     ];
 
     this.productService.getProductsWithOrdersSmall().then(data => this.products = data);
+
+    this.cols = [
+      { field: 'product', header: 'Product' },
+      { field: 'price', header: 'Price' },
+      { field: 'category', header: 'Category' },
+      { field: 'rating', header: 'Reviews' },
+      { field: 'inventoryStatus', header: 'Status' }
+    ];
+
+    this.statuses = [
+        { label: 'INSTOCK', value: 'instock' },
+        { label: 'LOWSTOCK', value: 'lowstock' },
+        { label: 'OUTOFSTOCK', value: 'outofstock' }
+    ];
+
+      this.countryService.getCountries().then(countries => {
+        this.countries = countries;
+      });
   }
 
   onUpload(event: any) {
@@ -61,4 +124,119 @@ export class DeliveryNoteComponent implements OnInit {
     table.clear();
     this.filter.nativeElement.value = '';
   }
+
+  save(info: string) {
+    console.log(info);
+    if (info === 'Confeccion Manual') {
+      this.sectionVisibility.showManual = true;
+      this.sectionVisibility.showMasiva = false;
+    } else if (info === 'Confeccion Masiva') {
+      this.sectionVisibility.showManual = false;
+      this.sectionVisibility.showMasiva = true;
+    }
+  }
+
+  // Subtable Commands
+
+  openNew() {
+    this.product = {};
+    this.submitted = false;
+    this.productDialog = true;
+  }
+
+  deleteSelectedProducts() {
+      this.deleteProductsDialog = true;
+  }
+
+  editProduct(product: Product) {
+      this.product = { ...product };
+      this.productDialog = true;
+  }
+
+  deleteProduct(product: Product) {
+      this.deleteProductDialog = true;
+      this.product = { ...product };
+  }
+
+  confirmDeleteSelected() {
+      this.deleteProductsDialog = false;
+      this.products = this.products.filter(val => !this.selectedProducts.includes(val));
+      this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Products Deleted', life: 3000 });
+      this.selectedProducts = [];
+  }
+
+  confirmDelete() {
+      this.deleteProductDialog = false;
+      this.products = this.products.filter(val => val.id !== this.product.id);
+      this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Product Deleted', life: 3000 });
+      this.product = {};
+  }
+
+  hideDialog() {
+      this.productDialog = false;
+      this.submitted = false;
+  }
+
+  saveProduct() {
+      this.submitted = true;
+
+      if (this.product.name?.trim()) {
+          if (this.product.id) {
+              // @ts-ignore
+              this.product.inventoryStatus = this.product.inventoryStatus.value ? this.product.inventoryStatus.value : this.product.inventoryStatus;
+              this.products[this.findIndexById(this.product.id)] = this.product;
+              this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Product Updated', life: 3000 });
+          } else {
+              this.product.id = this.createId();
+              this.product.code = this.createId();
+              this.product.image = 'product-placeholder.svg';
+              // @ts-ignore
+              this.product.inventoryStatus = this.product.inventoryStatus ? this.product.inventoryStatus.value : 'INSTOCK';
+              this.products.push(this.product);
+              this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Product Created', life: 3000 });
+          }
+
+          this.products = [...this.products];
+          this.productDialog = false;
+          this.product = {};
+      }
+  }
+
+  findIndexById(id: string): number {
+      let index = -1;
+      for (let i = 0; i < this.products.length; i++) {
+          if (this.products[i].id === id) {
+              index = i;
+              break;
+          }
+      }
+
+      return index;
+  }
+
+  createId(): string {
+      let id = '';
+      const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      for (let i = 0; i < 5; i++) {
+          id += chars.charAt(Math.floor(Math.random() * chars.length));
+      }
+      return id;
+  }
+
+  onGlobalFilter(table: Table, event: Event) {
+      table.filterGlobal((event.target as HTMLInputElement).value, 'contains');
+  }
+
+  filterCountry(event: any) {
+    const filtered: any[] = [];
+    const query = event.query;
+    for (let i = 0; i < this.countries.length; i++) {
+        const country = this.countries[i];
+        if (country.name.toLowerCase().indexOf(query.toLowerCase()) == 0) {
+            filtered.push(country);
+        }
+    }
+
+    this.filteredCountries = filtered;
+}
 }
--- src/app/domain/artifacts/deliverynotes/component/delivery-note.module.ts
+++ src/app/domain/artifacts/deliverynotes/component/delivery-note.module.ts
@@ -1,24 +1,48 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { DeliveryNoteRoutingModule } from './delivery-note-routing.module';
 import { DeliveryNoteComponent } from './delivery-note.component';
 
 
 import { TableModule } from 'primeng/table';
-import { ToolbarModule } from 'primeng/toolbar';
-import { SplitButtonModule } from 'primeng/splitbutton';
 import { FileUploadModule } from 'primeng/fileupload';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { CalendarModule } from "primeng/calendar";
 import { ToastModule } from 'primeng/toast';
+import { ToolbarModule } from 'primeng/toolbar';
+import { RatingModule } from 'primeng/rating';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { AutoCompleteModule } from "primeng/autocomplete";
+import { DropdownModule } from 'primeng/dropdown';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { DialogModule } from 'primeng/dialog';
+import { SplitButtonModule } from 'primeng/splitbutton';
 
 @NgModule({
   imports: [
     CommonModule,
     DeliveryNoteRoutingModule,
-    ToolbarModule,
     SplitButtonModule,
+    TableModule,
     FileUploadModule,
+    FormsModule,
+    ButtonModule,
+    RippleModule,
+    CalendarModule,
     ToastModule,
-    TableModule
+    ToolbarModule,
+    RatingModule,
+    InputTextModule,
+    InputTextareaModule,
+    AutoCompleteModule,
+    DropdownModule,
+    RadioButtonModule,
+    InputNumberModule,
+    DialogModule
   ],
   declarations: [DeliveryNoteComponent]
 })
